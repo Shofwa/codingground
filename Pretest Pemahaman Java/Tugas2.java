427e965136de681807172048150066f338db7a92
Shofwa <gribiganisland@gmail.com>
2017-02-27 09:19:41 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas2.java
@@ -0,0 +1,12 @@
+public class Tugas2{
+    public static void main(String []args){
+        String[]nama = new String []{
+            "Shofwa", "Ika", "Husnul"
+        };
+        
+        System.out.println(nama[0]);
+        System.out.println(nama[1]);
+        System.out.println(nama[2]);
+        
+    }
+}
