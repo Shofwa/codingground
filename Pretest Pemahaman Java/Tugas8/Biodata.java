a5d053e072c459eefe246a9f7efacb08673f2421
Shofwa <gribiganisland@gmail.com>
2017-03-05 14:28:11 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas8/Biodata.java
@@ -0,0 +1,9 @@
+public class Biodata{
+	String nama = "Shofwatur Rohmah";
+	String alamat = "Demak";
+	int umur = 24;
+
+	void CetakBiodata(){
+		System.out.println("Nama : " + nama + "\nAlamat : " + alamat + "\nUmur : " + umur);
+	}
+}
