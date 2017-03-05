8aeec8501e0c670c3dc1d0d38d38311385174e28
Shofwa <gribiganisland@gmail.com>
2017-03-05 15:13:27 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas11/Mahasiswa.java
@@ -0,0 +1,8 @@
+public class Mahasiswa extends Biodata{
+	String nim = "141240000227";
+	String jurusan = "Teknik Informatika";
+
+	void CetakBiodata(){
+		System.out.println("Nama : " + nama + "\nAlamat : " + alamat + "\nUmur : " + umur + "\nNIM : " + nim + "\nJurusan : " + jurusan);
+	}
+}
